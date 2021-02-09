81df982d564db86c07ac9f4f970172f9e06ff51e
Jonathan Redeker <jonathanredeker@live.com>
2021-02-09 08:44:45 -0800

    Added a method to retrieve the paddle's y-coordinate in relation to its collision height.

--- src/include/paddle.h
+++ src/include/paddle.h
@@ -5,10 +5,11 @@
 #include <types.h>
 
 typedef struct {
-    UINT8 nb_top, nb_mid, nb_bot;
-    UINT8 x, y;
-    INT8 vy;
-    UINT8 w, h;
+    UINT8 nb_top, nb_mid, nb_bot; // Sprite IDs
+    UINT8 x, y; // Position
+    INT8 vy; // Velocity Y
+    UINT8 w, h; // True width and height
+    UINT8 collision_w, collision_h; // Collision width and height
 } Paddle;
 
 /** Gets a more accurate representation of of the Paddle's x position.
@@ -21,21 +22,33 @@ UINT8 get_paddle_virtual_x(Paddle* paddle, BOOLEAN flip) {
     if (flip) {
         return paddle->x;
     } else {
-        return paddle->w - paddle->w + paddle->x;
+        return paddle->w - paddle->collision_w + paddle->x;
     }
+}
+
+/** Gets a more accurate representation of of the Paddle's x position.
+ * @param paddle    The Paddle to calculate the virtual x for.
+ * @param flip      TRUE for the opponent's paddle and FALSE for the player's Paddle.
+ * 
+ * @return          The virtual x position of the Paddle.
+ */
+UINT8 get_paddle_virtual_y(Paddle* paddle) {
+    return paddle->y + ((paddle->w - paddle->collision_w) / 2);
 }
 
 Paddle player = {
     1, 2, 3, // Sprite IDs
     24, 80, // Position
-    1, // Y velocity
-    8, 24 // Width and height
+    1, // Velocity Y
+    8, 24, // True width and height
+    3, 24 // Collision width and height
 };
 
 Paddle opponent = {
     4, 5, 6, // Sprite IDs
     144, 80, // Position
-    1, // Y velocity
-    8, 24 // Width and height
+    1, // Velocity Y
+    8, 24, // True width and height
+    3, 24 // Collision width and height
 };
 #endif // PADDLE_H
