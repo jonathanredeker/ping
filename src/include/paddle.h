3ea6783333f1821697fd73b2926bab2dd6d54691
Jonathan Redeker <jonathanredeker@live.com>
2021-02-09 10:15:40 -0800

    Implemented initialization functions.

--- src/include/paddle.h
+++ src/include/paddle.h
@@ -36,19 +36,32 @@ UINT8 get_paddle_virtual_y(Paddle* paddle) {
     return paddle->y + ((paddle->w - paddle->collision_w) / 2);
 }
 
-Paddle player = {
+void init_paddle(Paddle* paddle, UINT8 nb_top, UINT8 nb_mid, UINT8 nb_bot, UINT8 x, UINT8 y, INT8 vy, UINT8 w, UINT8 h, UINT8 collision_w, UINT8 collision_h) {
+    paddle->nb_top = nb_top;
+    paddle->nb_mid = nb_mid;
+    paddle->nb_bot = nb_bot;
+    paddle->x = x;
+    paddle->y = y;
+    paddle->vy = vy;
+    paddle->w = w;
+    paddle->h = h;
+    paddle->collision_w = collision_w;
+    paddle->collision_h = collision_h;
+}
+
+/* Paddle player = {
     1, 2, 3, // Sprite IDs
     24, 80, // Position
     1, // Velocity Y
     8, 24, // True width and height
     3, 24 // Collision width and height
-};
+}; */
 
-Paddle opponent = {
+/* Paddle opponent = {
     4, 5, 6, // Sprite IDs
     144, 80, // Position
     1, // Velocity Y
     8, 24, // True width and height
     3, 24 // Collision width and height
-};
+}; */
 #endif // PADDLE_H
