fd9f666a1872e348e469b1fecddbfe702789ff2a
Jonathan Redeker <jonathanredeker@live.com>
2021-02-09 08:42:54 -0800

    Clarifying code.

--- src/include/ball.h
+++ src/include/ball.h
@@ -5,11 +5,16 @@
 // Local
 #include "util.h"
 
+#define BALL_DEFAULT_X 84
+#define BALL_DEFAULT_Y 92
+
 typedef struct {
-    UINT8 nb;
-    UINT8 x, y;
-    INT8 vx, vy;
-    UINT8 w, h;
+    UINT8 nb; // Sprite ID
+    UINT8 x, y; // Position
+    UINT8 last_x, last_y; // The ball's position in the last frame
+    INT8 vx, vy; // Velocity vector
+    UINT8 w, h; // True width and height
+    UINT8 collision_w, collision_h; // Collision width and height
 } Ball;
 
 /** Gets a more accurate representation of of the Ball's x position.
@@ -18,7 +23,7 @@ typedef struct {
  * @return          The virtual x position of the Ball.
  */
 UINT8 get_ball_virtual_x(Ball* ball) {
-    return ball->x + ((8 - ball->w) / 2);
+    return ball->x + ((ball->w - ball->collision_w) / 2);
 }
 
 /** Gets a more accurate representation of of the Ball's y position.
@@ -27,14 +32,16 @@ UINT8 get_ball_virtual_x(Ball* ball) {
  * @return          The virtual y position of the Ball.
  */
 UINT8 get_ball_virtual_y(Ball* ball) {
-    return ball->y + ((8 - ball->h) / 2);
+    return ball->y + ((ball->h - ball->collision_h) / 2);
 }
 
 Ball ball = {
     0, // Sprite ID
     BALL_DEFAULT_X, BALL_DEFAULT_Y, // Position
-    -1, -1, // Velocity
-    4, 4, // Width and height
+    0, 0,
+    -1, 0, // Velocity vector
+    8, 8, // True width and height
+    4, 4, // Collision width and height
 };
 
 #endif // BALL_H
