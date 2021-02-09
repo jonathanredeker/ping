c0d57869eeda724d5010a400d1061b3bcad84dac
Jonathan Redeker <jonathanredeker@live.com>
2021-02-09 13:12:02 -0800

    Formatted and commented.

--- src/include/ball.h
+++ src/include/ball.h
@@ -2,6 +2,7 @@
 #define BALL_H
 // Native
 #include <gb/gb.h>
+#include <gb/bgb_emu.h>
 // Local
 #include "util.h"
 
@@ -9,26 +10,36 @@
 #define BALL_DEFAULT_Y 92
 
 typedef struct {
-    UINT8 nb; // Sprite ID
-    UINT8 x, y; // Position
-    UINT8 last_x, last_y; // The ball's position in the last frame
-    INT8 vx, vy; // Velocity vector
-    UINT8 w, h; // True width and height
-    UINT8 collision_w, collision_h; // Collision width and height
+    UINT8 nb;                           // Sprite ID
+    UINT8 x, y;                         // Position
+    UINT8 last_x, last_y;               // The ball's position in the last frame
+    INT8 vx, vy;                        // Velocity vector
+    UINT8 w, h;                         // True width and height
+    UINT8 collision_w, collision_h;     // Collision width and height
 } Ball;
 
-/**
+/** Initializes a Ball struct.
  * 
+ * @param ball          The Ball struct to initalize
+ * @param nb            The sprite ID
+ * @param x             The ball's x position
+ * @param y             The ball's y position
+ * @param vx            The ball's x velocity
+ * @param vy            The ball's y velocity
+ * @param w             The ball's width
+ * @param h             The ball's height
+ * @param collision_w   The ball's collision box width
+ * @param collision_h   The ball's collision box height
  */
-void init_ball(Ball* ball, UINT8 nb, UINT8 x, UINT8 y, UINT8 last_x, 
-    UINT8 last_y, INT8 vx, INT8 vy, UINT8 w, UINT8 h, UINT8 collision_w, 
+void init_ball(Ball* ball, UINT8 nb, UINT8 x, UINT8 y, INT8 vx, INT8 vy, 
+    UINT8 w, UINT8 h, UINT8 collision_w, 
     UINT8 collision_h) 
 {
     ball->nb = nb;
     ball->x = x;
     ball->y = y;
-    ball->last_x = last_x;
-    ball->last_y = last_y;
+    ball->last_x = x;
+    ball->last_y = y;
     ball->vx = vx;
     ball->vy = vy;
     ball->w = w;
@@ -54,15 +65,4 @@ UINT8 get_ball_virtual_x(Ball* ball) {
 UINT8 get_ball_virtual_y(Ball* ball) {
     return ball->y + ((ball->h - ball->collision_h) / 2);
 }
-/* 
-
-Ball ball = {
-    0, // Sprite ID
-    BALL_DEFAULT_X, BALL_DEFAULT_Y, // Position
-    0, 0,
-    -1, 0, // Velocity vector
-    8, 8, // True width and height
-    4, 4, // Collision width and height
-}; */
-
 #endif // BALL_H
--- src/include/paddle.h
+++ src/include/paddle.h
@@ -5,18 +5,50 @@
 #include <types.h>
 
 typedef struct {
-    UINT8 nb_top, nb_mid, nb_bot; // Sprite IDs
-    UINT8 x, y; // Position
-    INT8 vy; // Velocity Y
-    UINT8 w, h; // True width and height
-    UINT8 collision_w, collision_h; // Collision width and height
+    UINT8 nb_top, nb_middle, nb_bottom; // Sprite IDs
+    UINT8 x, y;                         // Position
+    INT8 vy;                            // Velocity Y
+    UINT8 w, h;                         // True width and height
+    UINT8 collision_w, collision_h;     // Collision width and height
 } Paddle;
 
-/** Gets a more accurate representation of of the Paddle's x position.
- * @param paddle    The Paddle to calculate the virtual x for.
- * @param flip      TRUE for the opponent's paddle and FALSE for the player's Paddle.
+/** Initializes a Paddle struct.
  * 
- * @return          The virtual x position of the Paddle.
+ * @param paddle        The Paddle struct to initalize
+ * @param nb_top        The sprite ID for the top paddle sprite
+ * @param nb_middle     The sprite ID for the middle paddle sprite
+ * @param nb_bottom     The sprite ID for the bottom paddle sprite
+ * @param x             The paddle's x position
+ * @param y             The paddle's y position
+ * @param vy            The paddle's y velocity
+ * @param w             The paddle's width
+ * @param h             The paddle's height
+ * @param collision_w   The paddle's collision rect width
+ * @param collision_h   The paddle's collision rect height
+ */
+void init_paddle(Paddle* paddle, UINT8 nb_top, UINT8 nb_middle, UINT8 nb_bottom, 
+    UINT8 x, UINT8 y, INT8 vy, UINT8 w, UINT8 h, UINT8 collision_w, 
+    UINT8 collision_h) 
+{
+    paddle->nb_top = nb_top;
+    paddle->nb_middle = nb_middle;
+    paddle->nb_bottom = nb_bottom;
+    paddle->x = x;
+    paddle->y = y;
+    paddle->vy = vy;
+    paddle->w = w;
+    paddle->h = h;
+    paddle->collision_w = collision_w;
+    paddle->collision_h = collision_h;
+}
+
+/** Gets a more accurate representation of of the Paddle's x position.
+ * 
+ * @param paddle        The Paddle to calculate the virtual x for
+ * @param flip          TRUE for the opponent's paddle and FALSE for the 
+ *                      player's Paddle
+ * 
+ * @return              The virtual x position of the Paddle
  */
 UINT8 get_paddle_virtual_x(Paddle* paddle, BOOLEAN flip) {
     if (flip) {
@@ -27,41 +59,14 @@ UINT8 get_paddle_virtual_x(Paddle* paddle, BOOLEAN flip) {
 }
 
 /** Gets a more accurate representation of of the Paddle's x position.
- * @param paddle    The Paddle to calculate the virtual x for.
- * @param flip      TRUE for the opponent's paddle and FALSE for the player's Paddle.
  * 
- * @return          The virtual x position of the Paddle.
+ * @param paddle        The Paddle to calculate the virtual x for.
+ * @param flip          TRUE for the opponent's paddle and FALSE for the 
+ *                      player's Paddle.
+ * 
+ * @return              The virtual x position of the Paddle.
  */
 UINT8 get_paddle_virtual_y(Paddle* paddle) {
-    return paddle->y + ((paddle->w - paddle->collision_w) / 2);
+    return paddle->y + ((paddle->w - paddle->collision_w) / 2u);
 }
-
-void init_paddle(Paddle* paddle, UINT8 nb_top, UINT8 nb_mid, UINT8 nb_bot, UINT8 x, UINT8 y, INT8 vy, UINT8 w, UINT8 h, UINT8 collision_w, UINT8 collision_h) {
-    paddle->nb_top = nb_top;
-    paddle->nb_mid = nb_mid;
-    paddle->nb_bot = nb_bot;
-    paddle->x = x;
-    paddle->y = y;
-    paddle->vy = vy;
-    paddle->w = w;
-    paddle->h = h;
-    paddle->collision_w = collision_w;
-    paddle->collision_h = collision_h;
-}
-
-/* Paddle player = {
-    1, 2, 3, // Sprite IDs
-    24, 80, // Position
-    1, // Velocity Y
-    8, 24, // True width and height
-    3, 24 // Collision width and height
-}; */
-
-/* Paddle opponent = {
-    4, 5, 6, // Sprite IDs
-    144, 80, // Position
-    1, // Velocity Y
-    8, 24, // True width and height
-    3, 24 // Collision width and height
-}; */
 #endif // PADDLE_H
